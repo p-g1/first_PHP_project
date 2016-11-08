4c1d1ba5c5b5989b3375d096b77db021135b73c0
Pete <petergarvin1@gmail.com>
2016-11-08 21:07:36 +0000

    up to lesson 6

---
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Yoda\EventBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render(
+            'EventBundle:Default:index.html.twig',
+            array('name' => $name)
+        );
+    }
+}
